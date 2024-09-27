8259e973a73b748ff518ea545f06db1a32af7273
PiotrHic <piotrhic@gmail.com>
2024-09-27 18:35:14 +0200

    Create Dancer Controller. Closes #21.

---
+++ src/main/java/com/example/my_first_jpa_app/DancerController.java
@@ -0,0 +1,11 @@
+package com.example.my_first_jpa_app;
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/dancers")
+public class DancerController {
+}
