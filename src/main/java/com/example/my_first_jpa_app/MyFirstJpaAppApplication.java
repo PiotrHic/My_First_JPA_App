f52bf144899884585e3902ba7d8e71a7372ff19e
PiotrHic <piotrhic@gmail.com>
2024-09-28 18:26:36 +0200

    Customize Open API. Closes #35.

--- src/main/java/com/example/my_first_jpa_app/MyFirstJpaAppApplication.java
+++ src/main/java/com/example/my_first_jpa_app/MyFirstJpaAppApplication.java
@@ -1,9 +1,11 @@
 package com.example.my_first_jpa_app;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition
 public class MyFirstJpaAppApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/example/my_first_jpa_app/controller/DancerController.java
+++ src/main/java/com/example/my_first_jpa_app/controller/DancerController.java
@@ -4,6 +4,10 @@ import com.example.my_first_jpa_app.domain.Dancer;
 import com.example.my_first_jpa_app.domain.DancerDTO;
 import com.example.my_first_jpa_app.domain.DancerMapper;
 import com.example.my_first_jpa_app.service.DancerService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -22,25 +26,50 @@ public class DancerController {
 
     private final DancerMapper dancerMapper;
 
+    @Operation(summary = "It adds a new dancer to the database")
+    @ApiResponse(responseCode = "201",
+                description = "Add new Dancer to the database",
+                content = {@Content(mediaType =  "application/json")})
     @PostMapping
     ResponseEntity<DancerDTO> createDancer(@RequestBody DancerDTO dancerDTO){
         Dancer toSave = dancerService.createDancer(dancerMapper.dancerDTOToDancer(dancerDTO));
 
         return new ResponseEntity<>(dancerMapper.dancerToDancerDTO(toSave), HttpStatus.valueOf(201));
     }
-
+    @Operation(summary = "It updates dancer with the new data")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                        description = "Update dancer to the database",
+                        content = {@Content(mediaType =  "application/json")}),
+            @ApiResponse(responseCode = "404",
+                    description = "Dancer was not found in database",
+                    content = {@Content(mediaType =  "application/json")}),
+    })
     @PutMapping("/{dancerID}")
     ResponseEntity <DancerDTO> updateDancerById(@PathVariable("dancerID") Integer dancerID, @RequestBody DancerDTO dancerDTO) {
         Dancer updated = dancerService.updateDancer(dancerID, dancerMapper.dancerDTOToDancer(dancerDTO));
         return new ResponseEntity<>(dancerMapper.dancerToDancerDTO(updated), HttpStatus.OK);
     }
 
+    @Operation(summary = "It brings one dancer from the database")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Get one dancer from the database",
+                    content = {@Content(mediaType =  "application/json")}),
+            @ApiResponse(responseCode = "404",
+                    description = "Dancer was not found in database",
+                    content = {@Content(mediaType =  "application/json")}),
+    })
     @GetMapping("/{dancerID}")
     ResponseEntity <DancerDTO> getDancerById(@PathVariable("dancerID") Integer dancerID) {
         Dancer founded = dancerService.getDancer(dancerID);
         return new ResponseEntity<>(dancerMapper.dancerToDancerDTO(founded), HttpStatus.OK);
     }
 
+    @Operation(summary = "Takes all dancers from the database")
+    @ApiResponse(responseCode = "200",
+            description = "Gives all dancers from the database",
+            content = {@Content(mediaType =  "application/json")})
     @GetMapping
     ResponseEntity <List<DancerDTO>> getAllDancers()  throws DancerNotFoundException {
         List<DancerDTO> dancersDTO = dancerService
@@ -52,12 +81,25 @@ public class DancerController {
     }
 
 
+    @Operation(summary = "It deletes one dancer from the database")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200",
+                    description = "Deletes one dancer from the database",
+                    content = {@Content(mediaType =  "application/json")}),
+            @ApiResponse(responseCode = "404",
+                    description = "Dancer was not found in database",
+                    content = {@Content(mediaType =  "application/json")}),
+    })
     @DeleteMapping("/{dancerID}")
     ResponseEntity <DancerDTO> deleteDancerById(@PathVariable("dancerID") Integer dancerID) throws DancerNotFoundException {
         Dancer deleted = dancerService.deleteDancer(dancerID);
         return new ResponseEntity<>(dancerMapper.dancerToDancerDTO(deleted), HttpStatus.OK);
     }
 
+    @Operation(summary = "Deletes all dancers from the database")
+    @ApiResponse(responseCode = "200",
+            description = "Deletes all dancers from the database",
+            content = {@Content(mediaType =  "application/json")})
     @DeleteMapping()
     ResponseEntity <String> deleteAllDancers(){
         dancerService.deleteAllDancers();
